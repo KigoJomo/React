d22e4800ba5943ca7f0f85de385a6ac36366dd54
KigoJomo <kigojay684@gmail.com>
2024-06-17 09:39:49 +0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,93 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import message from "./person";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const myFirstElement = <h1>Hello World</h1>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Car{
+  constructor(name) {
+    this.brand = name;
+  }
+
+  present() {
+    return 'I have a ' + this.brand;
+  }
+}
+
+class Model extends Car{
+  constructor(name, mod) {
+    super(name);
+    this.model = mod;
+  }
+
+  show() {
+    return this.present() + ', it is a ' + this.model;
+  }
+}
+
+const CarComponent = () => {
+  const myCar = new Model("Ford", "Mustang");
+
+  return (
+    <div>
+      <h5>{myCar.show()}</h5>
+      <p>You should get a { myCar.brand } too!</p>
+    </div>
+  );
+};
+
+const myFruits = ['apple', 'banana', 'cherry', 'date']
+
+const otherFruits = ['pineapple', 'water melon', 'pawpaw', 'mango']
+
+const allFruits = [...myFruits, ...otherFruits];
+
+const FruitComponent = ({ title, fruits }) => {
+  return (
+    <div>
+      <h3>{title}</h3>
+      <ul>
+        {fruits.map((fruit, index) => (
+          <li key={index}>{ fruit }</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+// destructuring
+// const vehicles = ['Cybertruck', 'Chiron', '911']
+// const [electric, iec, hybrid] = vehicles;
+
+const vehicleOne = {
+  brand: 'Tesla',
+  model: 'Cybertruck',
+  type: 'SUV',
+  year: '2024',
+  color: 'silver'
+}
+
+function myVehicle({ type, color, brand, model, year }) {
+  const message = `My ${type} is a ${year} ${color} ${brand} ${model}.`
+  return message;
+}
+
+const VehicleComponent = ({ vehicle }) => {
+  return <p>{ vehicle }</p>
+}
+
+function App() {
+  return (
+    <div>
+      <CarComponent />
+      <FruitComponent title={"Some Fruits"} fruits={myFruits} />
+      <FruitComponent title={"Other Fruits"} fruits={otherFruits} />
+      <FruitComponent title={"All Fruits"} fruits={allFruits} />
+      <VehicleComponent vehicle={myVehicle(vehicleOne)}/>
+      <p>{ message() }</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(< App />)
